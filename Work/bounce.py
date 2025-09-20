a748c21620298543cdb5f61fea7e81eae5a5c407
arceafolio <3406696815@qq.com>
2025-09-20 21:49:22 +0800

    finish 1.introduction

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+tell = 60.0
+i = 1
+while i <= 10:
+    print(i,round(tell,4),)
+    tell *= 0.6
+    i += 1
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+def ReadFile(filename):
+    file = open(filename)
+    f = csv.reader(file)
+    header = next(f)
+    cost = 0
+    for line in f:
+        try:
+            #row = line.split(',')
+            cost = cost + int(line[1]) * float(line[2])
+        except ValueError:
+            print("Couldn't handle " , line)
+    # print(f'Total cost', cost)
+    file.close()
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = ReadFile(filename)
+print('Total cost', cost)
